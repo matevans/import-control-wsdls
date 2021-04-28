0dfdc5f6a71f96ddb03bc462f982396ba18b1752
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2021-04-28 12:56:01 +0100

    ICS2-1389 Upgrade dependencies & resolvers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "3.4.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "4.3.0",
     "org.apache.axis2" % "axis2-kernel" % "1.7.9"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "3.4.0" % Test,
-    "org.scalatest" %% "scalatest" % "3.2.5" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "4.3.0" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.5" % "test, it",
     "com.typesafe.play" %% "play-test" % current % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % "test, it",

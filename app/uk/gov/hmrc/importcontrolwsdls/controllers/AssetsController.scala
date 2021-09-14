6a36d4a3cdae2eb163f8534195c8fe4eaf3f794a
Chris Hill <26716179+ChrisHillCap@users.noreply.github.com>
2021-09-14 12:39:40 +0100

    Removed delay (#21)

--- app/uk/gov/hmrc/importcontrolwsdls/controllers/AssetsController.scala
+++ app/uk/gov/hmrc/importcontrolwsdls/controllers/AssetsController.scala
@@ -16,29 +16,22 @@
 
 package uk.gov.hmrc.importcontrolwsdls.controllers
 
-import akka.actor.ActorSystem
-import akka.pattern.after
 import com.google.inject.Singleton
 import controllers.Assets
-import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import uk.gov.hmrc.importcontrolwsdls.config.AppConfig
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class AssetsController @Inject()(
   cc: ControllerComponents,
-  actorSystem: ActorSystem,
-  appConfig: AppConfig,
   assets: Assets
 )(implicit val ec: ExecutionContext)
     extends BackendController(cc) {
 
   def at(file: String): Action[AnyContent] = Action.async { implicit request =>
-    after(appConfig.assetsDelay, actorSystem.scheduler) {
       assets.at(file)(request)
-    }
   }
 }
